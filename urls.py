6323435d7538941dd4234ad9b27e6dab2c8a32e3
Nisarga-Oracle <NISARGA.S.ANGADI@ORACLE.COM>
2026-08-24 17:13:18 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("", include("operations.urls")),
+    path("reports/", include("reporting.urls")),
+]
